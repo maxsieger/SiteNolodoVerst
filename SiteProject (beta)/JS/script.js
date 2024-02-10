9be9ad84780dda44ab33949e6933507ee7269418
maxsieger <maksnov10@mail.ru>
2024-02-10 21:50:10 +0300

    1111

---
+++ SiteProject (beta)/JS/script.js
@@ -0,0 +1,24 @@
+    let slideIndex = 0;
+
+    function showSlides() {
+    const slides = document.querySelectorAll('.slide');
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.transform = `translateX(-${slideIndex * 20}%)`;
+    }
+    }
+
+    document.querySelector('.prev').addEventListener('click', () => {
+    slideIndex--;
+    showSlides();
+    });
+
+    document.querySelector('.next').addEventListener('click', () => {
+    slideIndex++;
+    showSlides();
+    });
